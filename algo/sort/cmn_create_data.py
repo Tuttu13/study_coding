959c05fa72edbe160a5f7339eceae9d30c6a84c0
Tuttu_13 <willson30426@yahoo.co.jp>
2024-01-14 14:07:28 +0900

    共通テストデータ作成クラス　修正

--- algo/sort/cmn_create_data.py
+++ algo/sort/cmn_create_data.py
@@ -2,13 +2,13 @@ import random
 
 class create_data():
 
-    def randam_data():
+    def randam_data(start_num, end_num, len_num):
 
         test_data = []
 
-        while len(test_data) != 10:
+        while len(test_data) != len_num:
 
-            num = random.randint(0, 9)
+            num = random.randint(start_num, end_num)
 
             if num not in test_data:
                 test_data.append(num)
