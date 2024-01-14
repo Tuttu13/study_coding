37189dd40866488e8f14ae3603c541099aab6f32
Tuttu_13 <willson30426@yahoo.co.jp>
2024-01-15 05:49:17 +0900

    共通関数　追加

--- algo/sort/cmn_create_data.py
+++ algo/sort/cmn_create_data.py
@@ -14,3 +14,13 @@ class create_data():
                 test_data.append(num)
         
         return test_data
+    
+    def randam_not_dep_data(len_num):
+
+        test_data = []
+        while len(test_data) != len_num:
+            num = random.randint(1, 99)
+            if num not in test_data:
+                test_data.append(num)
+        
+        return test_data
